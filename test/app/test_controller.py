347bbf64fa47e4ffa529ecfbb07fa8de9b935b29
namgivu <namgivu@gmail.com>
2017-03-31 03:16:49 +0700

    testcase with mock

--- test/app/test_controller.py
+++ test/app/test_controller.py
@@ -1,6 +1,7 @@
 import unittest
 
 
+
 class TestController(unittest.TestCase):
 
 
@@ -33,4 +34,35 @@ class TestController(unittest.TestCase):
     self.assertTrue(len(d)==2)
     for i in d:
       self.assertIsNotNone(i['name'])
+
+
+  def test_app_try_model_withMocking(self):
+    #set up expected
+    from app.model.some_model import SomeModel
+    o1 = SomeModel() ; o1.name = 'abb'
+    o2 = SomeModel() ; o2.name = 'cdd'
+    EXPECTED = dict()
+    EXPECTED['d'] = [o1, o2]
+
+    #set up mocking
+    from mock import patch
+    with patch('app.model.some_model.SomeModel') as model_mock:
+      model_mock.query.all  \
+        .return_value = EXPECTED['d']
+
+      #region get output
+      r = self.testClient.get('/app_try_model')
+
+      #convert json to dict
+      from flask import json
+      d = json.loads(r.data)
+      #endregion get output
+
+    #assert actual vs expected
+    self.assertIsNotNone(d)
+    self.assertTrue(len(d)==2)
+    e1 = d[0]
+    e2 = d[1]
+    self.assertEqual(e1['name'], o1.name)
+    self.assertEqual(e2['name'], o2.name)
 
