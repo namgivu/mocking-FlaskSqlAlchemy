0c6c4120904ed28f6bca05b232242d47362cb47e
namgivu <namgivu@gmail.com>
2017-03-30 23:09:21 +0700

    app config & model usage @ db 0th

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
+from flask.ext.sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/mock-FlaskSqlAlchemy?charset=utf8'
+db.init_app(app)
+
+
 @app.route('/')
 def app_index():
   return 'Flask app index - hello!'
+
+
+@app.route('/app_try_model')
+def app_try_model():
+  from app.model.some_model import SomeModel
+  someModelALL = SomeModel.query.all()
+  d = [someModel.name for someModel in someModelALL]
+  return str(d)
