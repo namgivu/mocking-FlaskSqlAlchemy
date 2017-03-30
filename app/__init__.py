9eaeb3758d173cfe7146e8a8c33e12b079e6c13b
namgivu <namgivu@gmail.com>
2017-03-30 23:56:13 +0700

    fix pycharm warning

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/mock-FlaskSqlAlchemy?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
